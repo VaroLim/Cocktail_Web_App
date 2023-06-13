438be4f65f998c3d1ed5b3731f26a9f8dd9d607b
SergioBSP <sergiodominguezgarcia993@gmail.com>
2023-06-13 19:03:17 +0200

    refix getCocktailsCategories

--- src/services/storage/Cocktails.ts
+++ src/services/storage/Cocktails.ts
@@ -1,19 +1,15 @@
-import {Cocktail} from "../../models/Cocktail"
+import {Category} from "../../models/Category"
 
-export const COCKTAIL_ID = 'apiCocktails'
+export const COCKTAIL_ID = 'cocktailsCategories'
 export const FAV_COCKTAILS = 'favCocktails'
 
 
-export const getCachedCocktails = (): Cocktail[] => {
+export const getCachedCategories = (): Category[] => {
   const response = window.localStorage.getItem(COCKTAIL_ID)
   return response ? JSON.parse(response) : []
 }
 
-export const setCachedCocktails = (Cocktails: Cocktail[]) => {
+export const setCachedCategories = (Cocktails: Category[]) => {
   window.localStorage.setItem(COCKTAIL_ID, JSON.stringify(Cocktails))
 }
 
-export const addNasaPhotoToCached = (cocktail: Cocktail) => {
-  const cachedPhotos = getCachedCocktails()
-  setCachedCocktails([cocktail, ...cachedPhotos])
-}
